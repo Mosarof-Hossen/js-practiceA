171ccdcfd1b51db6a2f8f8d4c40ac82213edc7cb
Mosarof-Hossen <mmd58553@gmail.com>
2025-12-26 22:01:05 +0600

    second problem solve

--- practice-1.js
+++ practice-1.js
@@ -1,3 +1,15 @@
 const area=2060;
 const result=area/2; 
 console.log(result)
+
+//second problem
+const money=9999;
+if(money>=45000){
+    console.log("laptop")
+}
+else if(money>=10000){
+    console.log("Cycle")
+}
+else{
+    console.log("chocolet")
+}
